bb7adf872e08fa20bef0f714b17c0b2a84fac2a0
Ishu Anand <talenthub.noreply@gmail.com>
2026-01-07 21:17:01 +0530

    new

--- inneralign-ui/src/utils/analyzeHandwriting.js
+++ inneralign-ui/src/utils/analyzeHandwriting.js
@@ -1,16 +1,14 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://127.0.0.1:5000",
-});
+import API from "./api";
 
 export const analyzeHandwriting = async (imageFile) => {
   const formData = new FormData();
   formData.append("image", imageFile);
 
-  const res = await API.post("/analyze", formData, {
-    headers: { "Content-Type": "multipart/form-data" },
+  const response = await API.post("/analyze", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
   });
 
-  return res.data;
+  return response.data;
 };
